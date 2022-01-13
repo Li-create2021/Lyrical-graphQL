57d9fbbbe7fc026d6e6c5b19bd6b19149753c547
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-01-13 21:39:45 +0100

    setup client side server with ApolloClient and Provider

--- client/index.js
+++ client/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+//1. Import ApolloClient and Provider, wrap the react app with the provider
+//2.pass the client to the provider as a prop
+//pass in an empty configuration object
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
